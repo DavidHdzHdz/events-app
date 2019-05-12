9f5ab3437ea6688983e90ce181cf6c9e501d8181
David <david.hernandez.hernandez.contractor@bbva.com>
2019-05-11 22:50:27 -0500

    add NavBar component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1> React events app</h1>
-      <EventDashboard />
+    <div>
+      <NavBar></NavBar>
+      <Container className="main">
+        <EventDashboard />
+      </Container>
     </div>
   );
 }
